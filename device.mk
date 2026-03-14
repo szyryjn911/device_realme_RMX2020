c16ab008ecc21c9b76ccda377dd3dee08d553326
dpxd2 <itsdipinbox+github@gmail.com>
2026-03-15 01:18:16 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -270,9 +270,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     PowerOffAlarm
 
-# Priv-keys
--include vendor/infinity-priv/keys/keys.mk
-
 # Properties
 -include $(DEVICE_PATH)/configs/props/vendor_logging_prop.mk
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
