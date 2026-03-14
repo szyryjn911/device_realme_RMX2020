d3acd19c142e0d0de6e6f1b7fc90c4aec5c8c7e6
dpxd2 <itsdipinbox+github@gmail.com>
2026-03-15 02:16:36 +0530

    Update lineage_RMX2020.mk

--- lineage_RMX2020.mk
+++ lineage_RMX2020.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_RMX2020
