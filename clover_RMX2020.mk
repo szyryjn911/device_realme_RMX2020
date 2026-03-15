5b033074d51f4f845b04fde6a54d71a16b099739
dpxd2 <itsdipinbox+github@gmail.com>
2026-03-16 01:34:31 +0530

    Update clover_RMX2020.mk

--- clover_RMX2020.mk
+++ clover_RMX2020.mk
@@ -12,9 +12,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := clover_RMX2020
 PRODUCT_DEVICE := RMX2020
